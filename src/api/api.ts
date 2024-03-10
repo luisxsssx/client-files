a254c57b577a0c8d6959fa92aa49bf0e036682b2
luisxsssx <cd210902@gmail.com>
2024-03-09 20:28:43 -0700

    Merge changes from child branch to parent branch

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,12 +3,21 @@ const API_BASE_URL = "http://localhost:8080";
 export const endpoints = {
   files: {
     createDir: `${API_BASE_URL}/createDir`,
-    upload: (folderPaht: string) =>
-      `${API_BASE_URL}/upload?dir=${encodeURIComponent(folderPaht)}/`,
+    upload: (folderPath: string) =>
+      `${API_BASE_URL}/upload?dir=${encodeURIComponent(folderPath)}`,
     getFolders: `${API_BASE_URL}/getAllFiles`,
-    download: (filePath: string) =>
-      `${API_BASE_URL}/download/?file${encodeURIComponent(filePath)}`,
-    delete: (deleteFile: string) =>
-      `${API_BASE_URL}/deleteDir/${encodeURIComponent(deleteFile)}`,
+    getFiles: (getFilePath: string) =>
+      `${API_BASE_URL}/getFiles?dir=${encodeURIComponent(getFilePath)}`,
+    download: (folder: string, fileName: string) =>
+      `${API_BASE_URL}/download/${encodeURIComponent(
+        folder
+      )}/${encodeURIComponent(fileName)}`,
+
+    delete: (filename: string, dir: string) =>
+      `${API_BASE_URL}/deleteFile?filename=${encodeURIComponent(
+        filename
+      )}&dir=${encodeURIComponent(dir)}`,
+    deleteDir: (dir: string) =>
+      `${API_BASE_URL}/deleteDir?dir=${encodeURIComponent(dir)}`,
   },
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./components/home.tsx";
+import FolderContent from "./components/folderContent.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/folder/:folderName",
+    element: <FolderContent />,
   },
 ]);
 
